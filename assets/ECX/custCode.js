8b5ca4daa15bd72d7ab3a2388cb7551c28085e26
MDSCLLC <MDSCLLC@users.noreply.github.com>
2020-07-27 07:07:58 -0400

    Login Working

--- assets/ECX/custCode.js
+++ assets/ECX/custCode.js
@@ -17,24 +17,39 @@ function onSignInAction() {
     }
     sessionStorage.setItem("sLogin", login);
     var url = String.format('{0}Security.WebService/AuthenticationServiceRest.svc/login.post', Config.siteUrl);
+
     $.ajax({
-        url: url,
-        type: 'POST',
-        data: {
-            u: login,
-            p: password
-        },
-        success: function(token) {
-            //alert("Sucessfull login...");
-            sessionStorage.setItem("sToken", token);
-            loadUserCases(122);
-            window.location.replace("./index.html");
-        },
-        error: function(xhr, error) {
-            alert(error);
-        }
-    });
-    return false;
+      url: url,
+      type: 'POST',
+      data: {
+          u: login,
+          p: password
+      },
+      success: function(token) {
+        sessionStorage.setItem("sToken", token);
+        //loadUserCases(122);
+          swal({
+                  title: 'Success',
+                  text: 'You have successfully logged in',
+                  timer: 1000,
+                  type: 'success',
+                  showConfirmButton: false
+              },
+              function() {
+                  location.reload();
+              });
+      },
+      error: function(xhr, error) {
+        sessionStorage.setItem("sToken", "error");
+          swal({
+              title: 'Warning',
+              text: 'Invalid User or Password',
+              timer: 1000,
+              type: 'warning'
+          });
+      }
+  });
+//    return false;
 }
 
 function onSearchCaseData() {
